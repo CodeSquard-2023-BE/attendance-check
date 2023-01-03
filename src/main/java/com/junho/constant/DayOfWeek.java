c3b3920f779a021266d22ad663a0b006ad928302
juno-junho <ssmm0205@naver.com>
2023-01-03 17:02:08 +0900

    feat: create enum 'DayOfWeek'

---
+++ src/main/java/com/junho/constant/DayOfWeek.java
@@ -0,0 +1,36 @@
+package com.junho.constant;
+
+import java.util.Arrays;
+
+public enum DayOfWeek {
+    MONDAY("월요일"),
+    TUESDAY("화요일"),
+    WEDNESDAY("수요일"),
+    THURSDAY("목요일"),
+    FRIDAY("금요일"),
+    SATURDAY("토요일"),
+    SUNDAY("일요일"),
+    ;
+
+    private final String dayOfTheWeek;
+
+    DayOfWeek(String dayOfTheWeek) {
+        this.dayOfTheWeek = dayOfTheWeek;
+    }
+
+    public static DayOfWeek findByDay(String day) {
+        return Arrays.stream(DayOfWeek.values())
+                .filter(dayOfWeek -> dayOfWeek.isEqualTo(day))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 요일입니다."));
+    }
+
+    private boolean isEqualTo(String day) {
+        return dayOfTheWeek.equals(day);
+    }
+
+    @Override
+    public String toString() {
+        return dayOfTheWeek;
+    }
+}
